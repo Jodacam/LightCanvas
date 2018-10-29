99d64cb62b96fa903695578dbbed81d2ff3bf3c3
pabloski300 <pabloski300@hotmail.com>
2018-10-29 12:11:50 +0100

    Añadida desactivacion de sprites en el hit canvas

--- example/example.js
+++ example/example.js
@@ -1,4 +1,4 @@
-
+var hitNum = 0;
 window.addEventListener("load",function(){
             
     let realCanvas = new CanvasManager("mycanvas",1280,720);
@@ -8,8 +8,18 @@ window.addEventListener("load",function(){
     let flecha = new HitableObject("Flecha",new Vector2(0,50),"img/right-arrow.png",150,150);
     let flecha2 = new HitableObject("Flecha2",new Vector2(180,90),"img/right-arrow.png",150,150);
     flecha.velocity = new Vector2(0,0);
+
     flecha.OnClick = function(e){
        this.velocity = this.velocity.add(new Vector2(1,0));
+       hitNum+=1;
+
+       if(hitNum==2){
+        this.velocity = new Vector2(0,0);
+        hitNum = 0;
+        flecha.deactivate();
+        realCanvas.clickObjects.delete(flecha._hitColor);
+        var i = 0;
+       }
     }
     flecha2.velocity = new Vector2(0,0);
     realCanvas.AddObject(flecha,1);
