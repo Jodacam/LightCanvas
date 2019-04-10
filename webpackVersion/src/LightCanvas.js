4b239ed8ce8b02eb587c8b4b82cd79691d086050
Jose Daniel <Jodacam2@gmail.com>
2019-04-10 13:54:19 +0200

    Started webpck build, with multiple render context

---
+++ webpackVersion/src/LightCanvas.js
@@ -0,0 +1,10 @@
+//Main Class that handle everything. 
+class LigthCanvas 
+{
+    static Init() {
+        
+    }
+}
+
+LigthCanvas.RENDER_MODE_CANVAS = "2d";
+LigthCanvas.RENDER_MODE_WEBGL = "webgl";
